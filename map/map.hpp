0d4a88bbe4b0f483b1fcd1f803eef53a0f638909
Naerhy <naerhy@protonmail.com>
2022-10-06 18:17:57 +0200

    Finalize swap functions and use pointer to BST (map)

--- map/map.hpp
+++ map/map.hpp
@@ -64,24 +64,27 @@ namespace ft
 
 			explicit map(key_compare const& comp = key_compare(),
 					allocator_type const& alloc = allocator_type())
-				: _allocator(alloc), _comp(comp), _bst(_comp, _allocator), _size(0) {}
+				: _allocator(alloc), _comp(comp),
+					_bst(new BST<value_type, key_compare, allocator_type>(_comp, _allocator)), _size(0) {}
 
 			template <typename InputIt>
 			map(InputIt first, InputIt last, key_compare const& comp = key_compare(),
 					allocator_type const& alloc = allocator_type())
-				: _allocator(alloc), _comp(comp), _bst(_comp, _allocator), _size(0)
+				: _allocator(alloc), _comp(comp),
+					_bst(new BST<value_type, key_compare, allocator_type>(_comp, _allocator)), _size(0)
 			{
 				insert(first, last);
 			}
 
 			map(map const& copy)
-				: _allocator(copy._allocator), _comp(copy._comp), _bst(_comp, _allocator),
-				_size(copy._size)
+				: _allocator(copy._allocator), _comp(copy._comp),
+					_bst(new BST<value_type, key_compare, allocator_type>(_comp, _allocator)),
+					_size(copy._size)
 			{
 				insert(copy.begin(), copy.end());
 			}
 
-			~map(void) {}
+			~map(void) { delete _bst; }
 
 			map& operator=(map const& x)
 			{
@@ -97,21 +100,21 @@ namespace ft
 			*     ITERATORS     *
 			********************/
 
-			iterator begin(void) { return iterator(&_bst, _bst.minimum(_bst.getRoot())); }
+			iterator begin(void) { return iterator(_bst, _bst->minimum(_bst->getRoot())); }
 
 			const_iterator begin(void) const
 			{
-				return const_iterator(&_bst, _bst.minimum(_bst.getRoot()));
+				return const_iterator(_bst, _bst->minimum(_bst->getRoot()));
 			}
 
 			iterator end(void)
 			{
-				return iterator(&_bst, _bst.successor(_bst.maximum(_bst.getRoot())));
+				return iterator(_bst, _bst->successor(_bst->maximum(_bst->getRoot())));
 			}
 
 			const_iterator end(void) const
 			{
-				return const_iterator(&_bst, _bst.successor(_bst.maximum(_bst.getRoot())));
+				return const_iterator(_bst, _bst->successor(_bst->maximum(_bst->getRoot())));
 			}
 
 			/*reverse_iterator rbegin(void) {}
@@ -149,15 +152,15 @@ namespace ft
 
 			pair<iterator, bool> insert(value_type const& val)
 			{
-				Node<value_type>* s = _bst.search(val.first);
-				if (s == _bst.getNil())
+				Node<value_type>* s = _bst->search(val.first);
+				if (s == _bst->getNil())
 				{
-					_bst.insert(val);
+					_bst->insert(val);
 					_size++;
-					return pair<iterator, bool>(iterator(&_bst, _bst.search(val.first)), true);
+					return pair<iterator, bool>(iterator(_bst, _bst->search(val.first)), true);
 				}
 				else
-					return pair<iterator, bool>(iterator(&_bst, s), false);
+					return pair<iterator, bool>(iterator(_bst, s), false);
 			}
 
 			iterator insert(iterator position, value_type const& val)
@@ -177,18 +180,18 @@ namespace ft
 
 			void erase(iterator position)
 			{
-				_bst.remove(position->first);
+				_bst->remove(position->first);
 				_size--;
 			}
 
 			size_type erase(key_type const& k)
 			{
-				Node<value_type>* s = _bst.search(k);
-				if (s == _bst.getNil())
+				Node<value_type>* s = _bst->search(k);
+				if (s == _bst->getNil())
 					return 0;
 				else
 				{
-					_bst.remove(k);
+					_bst->remove(k);
 					_size--;
 					return 1;
 				}
@@ -203,11 +206,17 @@ namespace ft
 				}
 			}
 
-			// void swap(map& x) {}
+			void swap(map& x)
+			{
+				std::swap(_allocator, x._allocator);
+				std::swap(_comp, x._comp);
+				std::swap(_bst, x._bst);
+				std::swap(_size, x._size);
+			}
 
 			void clear(void)
 			{
-				_bst.destroy();
+				_bst->destroy();
 				_size = 0;
 			}
 
@@ -225,25 +234,25 @@ namespace ft
 
 			iterator find(key_type const& k)
 			{
-				Node<value_type>* s = _bst.search(k);
-				if (s == _bst.getNil())
+				Node<value_type>* s = _bst->search(k);
+				if (s == _bst->getNil())
 					return end();
 				else
-					return iterator(&_bst, s);
+					return iterator(_bst, s);
 			}
 
 			const_iterator find(key_type const& k) const
 			{
-				Node<value_type>* s = _bst.search(k);
-				if (s == _bst.getNil())
+				Node<value_type>* s = _bst->search(k);
+				if (s == _bst->getNil())
 					return end();
 				else
-					return const_iterator(&_bst, s);
+					return const_iterator(_bst, s);
 			}
 
 			size_type count(key_type const& k) const
 			{
-				if (_bst.search(k) == _bst.getNil())
+				if (_bst->search(k) == _bst->getNil())
 					return 0;
 				else
 					return 1;
@@ -254,7 +263,7 @@ namespace ft
 				for (const_iterator cit = begin(); cit != end(); cit++)
 				{
 					if (!(_comp(cit->first, k)))
-						return iterator(&_bst, _bst.search(cit->first));
+						return iterator(_bst, _bst->search(cit->first));
 				}
 				return end();
 			}
@@ -264,7 +273,7 @@ namespace ft
 				for (const_iterator cit = begin(); cit != end(); cit++)
 				{
 					if (!(_comp(cit->first, k)))
-						return const_iterator(&_bst, _bst.search(cit->first));
+						return const_iterator(_bst, _bst->search(cit->first));
 				}
 				return end();
 			}
@@ -274,7 +283,7 @@ namespace ft
 				for (const_iterator cit = begin(); cit != end(); cit++)
 				{
 					if (_comp(k, cit->first))
-						return iterator(&_bst, _bst.search(cit->first));
+						return iterator(_bst, _bst->search(cit->first));
 				}
 				return end();
 			}
@@ -284,7 +293,7 @@ namespace ft
 				for (const_iterator cit = begin(); cit != end(); cit++)
 				{
 					if (_comp(k, cit->first))
-						return const_iterator(&_bst, _bst.search(cit->first));
+						return const_iterator(_bst, _bst->search(cit->first));
 				}
 				return end();
 			}
@@ -312,7 +321,7 @@ namespace ft
 		private:
 			allocator_type _allocator;
 			key_compare _comp;
-			BST<value_type, key_compare, allocator_type> _bst;
+			BST<value_type, key_compare, allocator_type>* _bst;
 			size_type _size;
 	};
 
@@ -361,8 +370,8 @@ namespace ft
 		return !(lhs < rhs);
 	}
 
-	// template <typename Key, typename T, typename Compare, typename Alloc>
-	// void swap(map<Key, T, Compare, Alloc>& lhs, map<Key, T, Compare, Alloc>& rhs { lhs.swap(rhs); }
+	template <typename Key, typename T, typename Compare, typename Alloc>
+	void swap(map<Key, T, Compare, Alloc>& lhs, map<Key, T, Compare, Alloc>& rhs) { lhs.swap(rhs); }
 }
 
 #endif
